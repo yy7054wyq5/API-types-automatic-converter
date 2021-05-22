6a7141dcbea4f70186c670fd029805d46844323f
wuyang <349413799@qq.com>
2021-05-22 12:36:56 +0800

    feat: it work!

--- convert-result/api-types/get.todos.resbody.interface.ts
+++ convert-result/api-types/get.todos.resbody.interface.ts
@@ -1,6 +1,6 @@
 export interface GetTodosResbodyI {
-	userId: number;
-	id: number;
-	title: string;
-	completed: boolean;
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
 }
