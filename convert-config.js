33cf90b14d635c81198921a12cc5cd49117ca456
wuyang <wuyang@juwang.cn>
2021-06-09 16:52:41 +0800

    feat: 默认配置diff

--- convert-config.js
+++ convert-config.js
@@ -1,3 +1,21 @@
+const Ajv = require('ajv');
+function differ(current, old, oldTypes, oldSchema) {
+	// console.log('current', current, typeof current);
+	// console.log('old', old);
+	// console.log('oldTypes', oldTypes);
+	// console.log('oldSchema', oldSchema);
+	const ajv = new Ajv();
+	if (oldSchema && current) {
+		const validate = ajv.compile(oldSchema);
+		const valid = validate(current);
+		if (valid) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
 module.exports = {
 	/** 就是 http-proxy-middleware 的配置*/
 	proxy: {
@@ -8,11 +26,11 @@ module.exports = {
 		changeOrigin: true,
 		secure: false,
 	},
-	differ: null, // for update
+	differ: () => true, // for update
 	port: 5800,
 	enable: {
-		jsonSchema: false,
-		json: false,
+		jsonSchema: true,
+		json: true,
 	},
 	filePath: {
 		json: './sample/assets/api-json',
